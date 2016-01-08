ef8e6f8706f20fbcab281b25f239427a6b7cbddc
Caoyynew <841033655@qq.com>
2016-01-08 17:51:50 +0800

    Initial Commit

---
+++ MimamoruApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MimamoruApp
+//
+//  Created by totyu3 on 16/1/8.
+//  Copyright © 2016年 totyu3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MimamoruApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MimamoruApp
+//
+//  Created by totyu3 on 16/1/8.
+//  Copyright © 2016年 totyu3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
